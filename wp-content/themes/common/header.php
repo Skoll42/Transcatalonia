1aed48b4bcc9d2e5ac22cb3a38d055a4f21063b0
Skoll <akostko@gmail.com>
2017-05-12 17:39:40 +0300

    ga initial commit

--- wp-content/themes/common/header.php
+++ wp-content/themes/common/header.php
@@ -26,6 +26,16 @@
 	<link rel="stylesheet" type="text/css" href="<?php echo get_stylesheet_directory_uri(); ?>/css/styles.css?ver=<?php echo time();?>">
     <title><?php wp_title(''); ?></title>
     <?php wp_head(); ?>
+    <script>
+        (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+                (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+            m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+        })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+        ga('create', 'UA-99022959-1', 'auto');
+        ga('send', 'pageview');
+
+    </script>
 </head>
 <body <?php body_class(); ?>>
 
